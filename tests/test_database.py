c8b6244c12525fe4319c2e7e83a8a049a48580aa
Alois Klink <alois.klink@gmail.com>
2018-12-19 15:14:49 +0000

    Add addData test for file dbs

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,6 +4,7 @@ import functools
 import pathlib
 import random
 import sqlalchemy.exc
+import os.path
 
 import pytest
 from nqm.iotdatabase.database import Database
@@ -16,8 +17,8 @@ def json_dbinfo(jsonfilepath="tdx-schemas.json"):
 
 def schemas():
     return itertools.chain(
-        [{}],
         [x["schema"] for x in json_dbinfo()],
+        [{}]
     )
 
 @pytest.fixture(params=schemas())
@@ -48,7 +49,7 @@ def make_val(gen_type, number):
     elif typ in {SQL_TYPES.NUMERIC, SQL_TYPES.INTEGER, SQL_TYPES.REAL}:
         return number
     elif typ is SQL_TYPES.TEXT:
-        return f"TTTTTTTTTTTTTTTTTEXT{number:.2f}"
+        return f"randomText{number:.2f}"
     else:
         raise NotImplementedError(f"Huh, {typ} did not match anything...")
 
@@ -89,3 +90,20 @@ def test_insert_nonunique_error(inmemdb, unique_schema):
     repeatNum = random.randint(0, number)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
         inmemdb.addData([data[repeatNum]])
+
+def make_filedb(filepath):
+    return Database(filepath, "file", "w+")
+
+def test_file_db(tmpdir):
+    filedb = make_filedb(os.path.join(tmpdir, "testdb.sqlite"))
+    schema = next(schemas())
+    filedb.createDatabase(schema=schema)
+    number = 100
+    data = make_data(schema, number=number)
+    assert filedb.addData(data) == {"count": number}
+
+    # reload database
+    filedb = make_filedb(os.path.join(tmpdir, "testdb.sqlite"))
+    filedb.createDatabase(schema=schema)
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        filedb.addData([data[0]]) # should cause uniqueIndex error
