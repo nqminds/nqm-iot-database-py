7b2f6313ee421112ed62af6bfe46b148dbe47ba6
Alexandru <mereacre@gmail.com>
2019-01-15 15:44:58 +0000

    Changed references

--- nqm_iot_database/__init__.py
+++ nqm_iot_database/__init__.py
@@ -1,1 +1,6 @@
-
+from .database import Database
+from . import _sqliteconstants
+from . import _sqliteutils
+from . import _sqliteinfotable
+from . import _sqliteschemaconverter
+from . import _sqlitealchemyconverter
