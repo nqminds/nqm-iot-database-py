d7fb2c3c9a9c8938b5991dd3dc29fba7eccbe40f
Alexandru <mereacre@gmail.com>
2019-01-15 15:05:49 +0000

    changed project structure+add setup cont

---
+++ setup.py
@@ -0,0 +1,13 @@
+import io
+from setuptools import setup
+
+setup(
+    name="nqm_iot_database",
+    version="1.0.0",
+    author="Alois Klink",
+    author_email="alois.klink@gmail.com",
+    packages=["nqm_iot_database"],
+    url="https://github.com/nqminds/nqm-iot-database-py",
+    description="Python port of nqminds/nqm-iot-database-utils",
+    install_requires=["sqlalchemy", "sqlite3", "mongosql", "tempfile"]
+)
