36083e1c717d39fab3e9d2d0ad4042ca0f498d3b
Alexandru <mereacre@gmail.com>
2019-01-15 15:14:04 +0000

    Changed install requirements

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     packages=["nqm_iot_database"],
     url="https://github.com/nqminds/nqm-iot-database-py",
     description="Python port of nqminds/nqm-iot-database-utils",
-    install_requires=["sqlalchemy", "mongosql", "tempfile"]
+    install_requires=["sqlalchemy", "mongosql"]
 )
