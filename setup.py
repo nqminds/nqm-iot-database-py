9bb4b9b430f1ff8baadb5115b2db0ba2b31a4e3f
Alois Klink <alois.klink@gmail.com>
2019-01-21 12:03:31 +0000

    Version using module __version__ string
    Also updates metadata of package.

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+# required for gets the current version of the package
+import nqm.iotdatabase
+
 setup(
     name='nqm-iot-database-py',
-    version=PipMethods.find_version("nqm", "iotdatabase", "__init__.py"),
+    version=nqm.iotdatabase.__version__,
     packages=['nqm.iotdatabase'],
     author='Alois Klink',
     author_email='alois.klink@gmail.com',
-    install_requires=['sqlalchemy', 'mongosql', 'shortuuid', 'numpy', 'future']
+    description="Library for accessing a local nqm-iot-database",
+    include_package_data=True,
+    python_requires=">=3.6",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nqminds/nqm-iot-database-py',
+    install_requires=['sqlalchemy', 'mongosql', 'shortuuid', 'numpy', 'future'],
+    zip_safe=True,
 )
