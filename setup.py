912cac18a7aa03cb04ef6a633431ace3297f4ac5
Alois Klink <alois.klink@gmail.com>
2019-01-21 12:01:54 +0000

    Indent with 4-spaces instead of tabs
    Damn you PEP8 for forcing me to use spaces instead of the superior
    tabs!

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-	name='nqm-iot-database-py',
-	version='1',
-	packages=['nqm.iotdatabase'],
-	author='Alois Klink',
-	author_email='alois.klink@gmail.com',
-	install_requires=['sqlalchemy', 'mongosql', 'shortuuid', 'numpy', 'future']
+    name='nqm-iot-database-py',
+    version=PipMethods.find_version("nqm", "iotdatabase", "__init__.py"),
+    packages=['nqm.iotdatabase'],
+    author='Alois Klink',
+    author_email='alois.klink@gmail.com',
+    install_requires=['sqlalchemy', 'mongosql', 'shortuuid', 'numpy', 'future']
 )
