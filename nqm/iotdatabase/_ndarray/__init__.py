ee979a1930332856a4e78e9ae03e45b68d822f84
Alois Klink <alois.klink@gmail.com>
2019-01-23 14:23:04 +0000

    Add ability to save NDArrays as Base64 strings

---
+++ nqm/iotdatabase/_ndarray/__init__.py
@@ -0,0 +1,3 @@
+"""Module for loading/storing n-dimensional arrays."""
+from nqm.iotdatabase._ndarray.ndarray import NDArray
+from nqm.iotdatabase._ndarray.fileio import *
--- tests/test_ndarray.py
+++ tests/test_ndarray.py
@@ -1,13 +1,14 @@
 import pytest
 import nqm.iotdatabase._ndarray as _ndarray
 import numpy as np
+import os
 
 types = ["int8", "int64", "uint32", "float", "double"]
 @pytest.fixture(params=types)
 def dtype(request):
     return request.param
 
-shapes = [[i] * j for i in range(1,3) for j in range(6)]
+shapes = [(2, 3, 4), (5), (1, 1, 1)]
 @pytest.fixture(params=shapes)
 def shape(request):
     return request.param
@@ -16,12 +17,25 @@ def shape(request):
 def c_order(request):
     return request.param
 
-def test_saving_loading(dtype, shape, c_order, tmp_path):
+@pytest.fixture(params=("f", "B", "G"))
+def fileformat(request):
+    return request.param
+
+@pytest.fixture(scope="module") # run once per module
+def datadir(tmpdir_factory):
+    datadir = tmpdir_factory.mktemp("test_ndarray_data")
+    os.makedirs(datadir, exist_ok=True)
+    return datadir
+
+def test_saving_loading(dtype, shape, c_order, fileformat, datadir):
     a = np.random.random_sample(shape)
     if c_order: a = np.ascontiguousarray(a, np.dtype(dtype))
     else: a = np.asfortranarray(a, np.dtype(dtype))
 
-    filepath = "./test"
-    metadata = _ndarray.saveNDArray(a, filepath, tmp_path)
+    metadata1 = _ndarray.saveNDArray(a, datadir, storage_method=fileformat)
+    metadata2 = _ndarray.saveNDArray(a+2, datadir, storage_method=fileformat)
 
-    assert np.array_equal(a, _ndarray.getNDArray(metadata, tmp_path))
+    assert np.array_equal(a, _ndarray.getNDArray(metadata1, datadir))
+    a2 = _ndarray.getNDArray(metadata2, datadir)
+    assert not np.array_equal(a, a2)
+    assert np.array_equal(a+2, a2)
