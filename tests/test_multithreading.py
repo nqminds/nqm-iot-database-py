413db404b53b2bd8fc54bdc8b0252d296bdae2b0
Alois Klink <alois@nquiringminds.com>
2019-02-06 12:46:56 +0000

    Add test for multithreaded getData

--- tests/test_multithreading.py
+++ tests/test_multithreading.py
@@ -60,3 +60,43 @@ def test_make_multithreaded_db(make_data, tmp_path, row_equal):
     assert len(data) == len(loaded_data)
     for datum, loaded_datum in zip(data, loaded_data):
         row_equal(datum, loaded_datum)
+
+class GetDataFromDb(threading.Thread):
+    def __init__(self, path, filter, options):
+        self.path = path
+        self.filter = filter
+        self.options = options
+        self.res = None
+        super().__init__()
+
+    def run(self):
+        db, key = make_ndarray_db(self.path)
+        self.res = db.getData(self.filter, options=self.options)
+
+def test_get_multithreaded_db(make_data, tmp_path, row_equal):
+    number = 100
+    schema = ndarray_schema()
+    data = make_data(schema, number)
+    output = [None,] * no_threads
+    step = len(data)/no_threads
+    path = tmp_path / "multithreading_test.sqlite"
+
+    db, key = make_ndarray_db(path)
+    db.addData(data)
+
+    data = sorted(data, key=lambda x: x[key])
+    thread_data = [data[int(x*step): int((x+1) * step)] for x in range(no_threads)]
+
+    thread_data_key = [{"$or": [{key: row[key]} for row in data]} for data in thread_data]
+
+    threads = [GetDataFromDb(path, filter=data_key, options={"sort": {key: 1}})
+        for data_key in thread_data_key ]
+    for thread in threads:
+        thread.start()
+
+    for expected_thread_data, thread in zip(thread_data, threads):
+        thread.join()
+        loaded_thread_data = thread.res.data
+        assert len(expected_thread_data) == len(loaded_thread_data)
+        for datum, loaded_datum in zip(expected_thread_data, loaded_thread_data):
+            row_equal(datum, loaded_datum)
