32a4a93da946057570e462e9e3c026f49c9d3f39
Alois Klink <alois.klink@gmail.com>
2019-05-01 18:45:26 +0100

    Rebase to remove some Pylint issues.
    Need to do a further rebase in the future.

--- nqm/iotdatabase/ndarray/fileio.py
+++ nqm/iotdatabase/ndarray/fileio.py
@@ -1,15 +1,21 @@
 """Stores functions about storing ndarrays in dbs.
 """
 import typing as ty
+from os import PathLike
 import numpy as np # only for typing
 
 from .ndarray import NDArray
-from .storageformats import storage_types
+from .storageformats import STORAGE_TYPES
+
+Path = ty.Union[PathLike, ty.Text]
+
+#TODO: Rename to match PEP8?
+#pylint: disable=locally-disabled, invalid-name
 
 def saveNDArray(
-    array: np.ndarray,
-    relative_loc="",
-    storage_method="",
+        array: np.ndarray,
+        relative_loc: Path = "",
+        storage_method: ty.Text = "",
 ):
     """Saves an array.
 
@@ -23,33 +29,36 @@ def saveNDArray(
     if not storage_method:
         storage_method = "f"
     try:
-        storage_class = storage_types[storage_method]
+        storage_class = STORAGE_TYPES[storage_method]
     except:
         raise NotImplementedError(
             f"Loading NDArray with version {storage_method} failed!"
-            f"Only versions {storage_types.keys()} are supported.")
-    return storage_class.save(array,relative_loc)
+            f"Only versions {STORAGE_TYPES.keys()} are supported.")
+    return storage_class.save(array, relative_loc)
 
-def getNDArray(metadata: NDArray, relative_loc="") -> np.ndarray:
+def getNDArray(
+        metadata: NDArray,
+        relative_loc: Path = ""
+) -> np.ndarray:
     """Opens an NDArray object as a numpy array
 
     Args:
         metadata: The object containing the array metadata.
         relative_loc: Relative location of any filepaths.
-    
+
     Returns:
         A numpy array.
     """
     storage_class = None
     try:
-        storage_class = storage_types[metadata.v]
+        storage_class = STORAGE_TYPES[metadata.v]
     except:
         raise NotImplementedError(
             f"Loading NDArray with version {metadata.v} failed!"
-            f"Only versions {storage_types.keys()} are supported.")
-    return storage_class.get(metadata,relative_loc)
+            f"Only versions {STORAGE_TYPES.keys()} are supported.")
+    return storage_class.get(metadata, relative_loc)
 
-def deleteNDArray(metadata: NDArray, relative_loc=""):
+def deleteNDArray(metadata: NDArray, relative_loc: Path = "") -> None:
     """Deletes the given NDArray.
 
     Args:
@@ -58,9 +67,9 @@ def deleteNDArray(metadata: NDArray, relative_loc=""):
     """
     storage_class = None
     try:
-        storage_class = storage_types[metadata.v]
+        storage_class = STORAGE_TYPES[metadata.v]
     except:
         raise NotImplementedError(
             f"Deleting NDArray with version {metadata.v} failed!"
-            f"Only versions {storage_types.keys()} are supported.")
+            f"Only versions {STORAGE_TYPES.keys()} are supported.")
     return storage_class.delete(metadata, relative_loc)
--- nqm/iotdatabase/ndarray/ndarray.py
+++ nqm/iotdatabase/ndarray/ndarray.py
@@ -3,30 +3,41 @@
 This can be used to store an NDArray as a JSON object.
 """
 import typing as ty
-import numpy as np # only used for typing
 import json
+import numpy as np # only used for typing
 
 np_type_to_single_char: ty.Dict[ty.Text, np.dtype] = {
-    v: c for c, v in np.sctypeDict.items() 
+    v: c for c, v in np.sctypeDict.items()
     if isinstance(c, str) and len(c) == 1
 }
 """Dict of single-chars to numpy types, ie p is an int64, P uint64"""
 
-varlength_types = {"O", "U", "V"}
+VARLENGTH_TYPE_CHARS = {"O", "U", "V"}
 """Set of Numpy types that require a length to be specified"""
 
 def basictypestring(
-    charBuiltinType: ty.Text,
-    length: int = -1,
-    byteorder: ty.Text = "="
-) -> ty.Text:
-    # only specify length if we have a variable width type
-    l = str(length) if charBuiltinType in varlength_types else ""
-    # only specify byte order if we need to
-    b = byteorder if byteorder != "=" and byteorder != "|" else ""
-    return f"{b}{charBuiltinType}{l}"
+        dtype_char: ty.Text,
+        length: int = -1,
+        byteorder: ty.Text = "="
+    ) -> ty.Text:
+    """Creates a compressed typestring from a given Numpy type details.
 
-class NDArray(object):
+    Arguments:
+        dtype_char: A single char from Numpy. See `numpy.dtype.char`.
+        length: The length of the dtype in bytes. See `numpy.dtype.itemsize`
+        byteorder:
+            The byte order, ie little/big-endian. See `numpy.dtype.byteorder`
+
+    Returns:
+        A numpy typestring that is as compressed as possible.
+    """
+    # only specify length if we have a variable width type
+    lengthstr = str(length) if dtype_char in VARLENGTH_TYPE_CHARS else ""
+    # only specify byte order if we need to
+    byteorderchar = byteorder if byteorder not in ("=", "|") else ""
+    return f"{byteorderchar}{dtype_char}{lengthstr}"
+
+class NDArray():
     """The metadata of an NDArray.
 
     Example:
@@ -75,7 +86,7 @@ class NDArray(object):
             version:
                 The version of the metadata.
                 "f" means the pointer is a filepath to a raw binary blob.
-        
+
         Returns:
             The created NDArray metadata object.
         """
@@ -84,15 +95,17 @@ class NDArray(object):
         typestr = basictypestring(dtype.char, dtype.itemsize, dtype.byteorder)
         return cls(t=typestr, s=array.shape, v=version, p=pointer, c=c_order)
 
-    def __init__(self,
-        t: ty.Text = "V", # void data type
-        s: ty.Iterable[int] = tuple(), # no dimensions
-        v: ty.Text = "", # assume no version
-        p: ty.Text = "", # assume no pointer
-        c: bool = True, # column order, True for c-type column ordering
+    #pylint: disable=locally-disabled, too-many-arguments
+    def __init__(
+            self,
+            t: ty.Text = "V", # void data type
+            s: ty.Iterable[int] = tuple(), # no dimensions
+            v: ty.Text = "", # assume no version
+            p: ty.Text = "", # assume no pointer
+            c: bool = True, # column order, True for c-type column ordering
     ):
         """Constructs a new NDArray object
-        
+
         Args:
             t: typestr, basic string format is endian, type, byte size.
                 ie ``>i8`` is an 8-bit signed big-endian int
@@ -102,17 +115,22 @@ class NDArray(object):
             p: the pointer to the location of the data. Usually a filepath.
             c: True if using C-type column ordering
         """
+        #pylint: disable=locally-disabled, multiple-statements, invalid-name
         m = self
         m.t = t; m.s = s; m.v = v; m.p = p; m.c = c
 
     def todict(self) -> dict:
+        """Returns this object as a dict"""
         return_dict = {"t": self.t, "s": self.s, "v": self.v, "p": self.p}
-        if not self.c: return_dict["c"] = self.c
+        if not self.c:
+            return_dict["c"] = self.c
         return return_dict
 
     def tojson(self) -> ty.Text:
+        """Returns this object as a json"""
         return json.dumps(self.todict())
 
     @classmethod
-    def fromjson(cls, jsonstr) -> "NDArray":
+    def fromjson(cls, jsonstr: ty.Text) -> "NDArray":
+        """Creates an NDArray object from a json."""
         return cls(**json.loads(jsonstr))
